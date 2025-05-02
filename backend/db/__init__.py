4a41db6bed802e69ace43f09673f4ffb433e75dc
Aldik15 <serikaldiar033@gmail.com>
2025-05-03 02:01:22 +0600

    Reverted to sayka08/diplom state

--- backend/db/__init__.py
+++
@@ -1,2 +0,0 @@
-from backend.db.base import Base  # Чтобы Alembic нашел Base
-from backend.db.models import comment, follow, like, post, user  # Импорт всех моделей
--- backend/schemas/like.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class LikeResponse(BaseModel):
-    post_id: int
-    likes_count: int
-
-    class Config:
-        orm_mode = True
